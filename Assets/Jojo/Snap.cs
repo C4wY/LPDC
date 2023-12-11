6a97b2701d18b99a0a10581a51fed7e8171c08d6
jniac <joseph.merdrignac@gmail.com>
2023-12-11 11:17:21 +0100

    snap + free

--- Assets/Jojo/Snap.cs
+++ Assets/Jojo/Snap.cs
@@ -24,15 +24,18 @@ public class Snap : MonoBehaviour
 
     void SnapUpdate()
     {
-        var p = transform.localPosition;
-        var s = transform.localScale;
-        var scalar = StepToFloat(step);
-        var min = (Vector3)Vector3Int.RoundToInt((p - s / 2) / scalar) * scalar;
-        var max = (Vector3)Vector3Int.RoundToInt((p + Vector3.one * 0.01f + s / 2) / scalar) * scalar;
-        var bounds = new Bounds((min + max) / 2, max - min);
-        transform.localPosition = bounds.center;
-        transform.localScale = bounds.size;
-        transform.rotation = Quaternion.identity;
+        if (step != Step.Free)
+        {
+            var p = transform.localPosition;
+            var s = transform.localScale;
+            var scalar = StepToFloat(step);
+            var min = (Vector3)Vector3Int.RoundToInt((p - s / 2) / scalar) * scalar;
+            var max = (Vector3)Vector3Int.RoundToInt((p + Vector3.one * 0.01f + s / 2) / scalar) * scalar;
+            var bounds = new Bounds((min + max) / 2, max - min);
+            transform.localPosition = bounds.center;
+            transform.localScale = bounds.size;
+            transform.rotation = Quaternion.identity;
+        }
     }
 
     // Update is called once per frame
