307e1588444dbdbb85697529ae63b1e115c4d0a4
jniac <joseph.merdrignac@gmail.com>
2023-12-11 11:12:38 +0100

    snap + step

--- Assets/Jojo/Snap.cs
+++ Assets/Jojo/Snap.cs
@@ -15,7 +15,9 @@ public class Snap : MonoBehaviour
 
     public static float StepToFloat(Step step) => step switch
     {
-
+        Step.Unit => 1,
+        Step.Half => .5f,
+        Step.Quarter => .25f,
     };
 
     public Step step = Step.Unit;
@@ -24,9 +26,10 @@ public class Snap : MonoBehaviour
     {
         var p = transform.localPosition;
         var s = transform.localScale;
-        var min = Vector3Int.RoundToInt(p - s / 2);
-        var max = Vector3Int.RoundToInt(p + Vector3.one * 0.01f + s / 2);
-        var bounds = new BoundsInt(min, max - min);
+        var scalar = StepToFloat(step);
+        var min = (Vector3)Vector3Int.RoundToInt((p - s / 2) / scalar) * scalar;
+        var max = (Vector3)Vector3Int.RoundToInt((p + Vector3.one * 0.01f + s / 2) / scalar) * scalar;
+        var bounds = new Bounds((min + max) / 2, max - min);
         transform.localPosition = bounds.center;
         transform.localScale = bounds.size;
         transform.rotation = Quaternion.identity;
