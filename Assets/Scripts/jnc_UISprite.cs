d63af99a87893ee4e3464c05942aa77a338013f6
jniac <joseph.merdrignac@gmail.com>
2024-03-04 12:02:52 +0100

    UI Shader for Kiiiiira

---
+++ Assets/Scripts/jnc_UISprite.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+public class jnc_UISprite : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float overrideAlpha = 0;
+
+    public Color overrideColor = Color.white;
+
+    void Update()
+    {
+        if (TryGetComponent<SpriteRenderer>(out var spriteRenderer))
+        {
+            spriteRenderer.material.SetColor("_OverrideColor", overrideColor);
+            spriteRenderer.material.SetFloat("_OverrideAlpha", overrideAlpha);
+        }
+
+        if (TryGetComponent<Image>(out var image))
+        {
+            image.material.SetColor("_OverrideColor", overrideColor);
+            image.material.SetFloat("_OverrideAlpha", overrideAlpha);
+        }
+    }
+}
