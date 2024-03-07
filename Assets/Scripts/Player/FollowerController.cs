7cb46459178457d8669b92ab24efa2a1c27a1c42
jniac <joseph.merdrignac@gmail.com>
2024-03-07 13:58:26 +0100

    wait, is "Player" a relevant name here?

--- Assets/Scripts/Player/FollowerController.cs
+++ Assets/Scripts/Player/FollowerController.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 namespace Player
 {
+    public class FollowerControllerParameters
+    {
+        public float distanceToLeaderMin = 1.0f;
+        public float distanceToLeaderMax = 6.0f;
+    }
+
     public class FollowerController : MonoBehaviour
     {
+        Player player, leaderPlayer;
 
+        void FixedUpdate()
+        {
+
+        }
     }
 }
 
