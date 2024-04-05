39d686b88055f98a877202b1c9cb8a3c7cbbebda
UtopisteNJoly <nicolasnicolas.joly@supdecreation.com>
2024-04-05 17:15:12 +0200

    Dash invincible : SANTE ET MOVE CHANGÉS

--- Assets/Scripts/Avatar/Santé.cs
+++ Assets/Scripts/Avatar/Santé.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Avatar
 {
     public class Santé : MonoBehaviour
     {
+
+        public int viemax;
+        public float compteurInvincibilité;
+
         public int PV = 3;
 
+        void Update()
+        {
+            if (compteurInvincibilité > 0)
+            {
+                compteurInvincibilité -= Time.deltaTime;
+
+            }
+        }
         public void FaireDégâts(int dégâts = 1)
         {
-            PV += -dégâts;
+            if (compteurInvincibilité <= 0)
 
-            if (PV == 0)
             {
-                Debug.Log("Jeu fini");
-                Debug.Break();
+
+                Debug.Log("OUCH");
+                PV += -dégâts;
+
+                if (PV == 0)
+                {
+                    Debug.Log("Jeu fini");
+                    Debug.Break();
+                }
             }
         }
     }
