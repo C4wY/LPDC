9268e868dab8f110ded666fa10cd5b4fc24ea3c8
CawY <ozennenathan@gmail.com>
2024-11-19 17:16:41 +0100

    tp hub

--- Assets/Collaborateurs/Caw_Nathan/TP_hub.cs
+++ Assets/Collaborateurs/Caw_Nathan/TP_hub.cs
@@ -1,17 +1,32 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class Portal : MonoBehaviour
 {
-    [SerializeField] private string Biz_Lobby;
+    [SerializeField] private Object targetScene;
+
+    private string sceneName;
     private bool isPlayerInPortal = false;
+
+    private void Start()
+    {
+#if UNITY_EDITOR
+        if (targetScene != null && targetScene is SceneAsset sceneAsset)
+        {
+            sceneName = sceneAsset.name;
+        }
+#endif
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             isPlayerInPortal = true;
-            Debug.Log("Appuyez sur F pour entrer dans le portail");
         }
     }
 
@@ -33,7 +48,13 @@ public class Portal : MonoBehaviour
 
     private void LoadTargetScene()
     {
-        Debug.Log("Chargement scene : " + Biz_Lobby);
-        SceneManager.LoadScene(Biz_Lobby);
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogWarning("Aucune cible");
+        }
     }
 }
