c925f67b115b3ca82b0a1e5cd82c1afcf533bc66
CawY <ozennenathan@gmail.com>
2024-12-10 18:21:09 +0100

    Dessolution de l'assemblé national ( des fragments de tronc )

--- Assets/Collaborateurs/Unj_Dossier/Unj-Scripts/Unj_Cassable.cs
+++ Assets/Collaborateurs/Unj_Dossier/Unj-Scripts/Unj_Cassable.cs
@@ -17,12 +17,11 @@ public class Unj_Cassable : MonoBehaviour
 
             if (ausecours.IsDashing)
             {
+                Debug.Log("Collision avec le joueur, explosion déclenchée");
                 Explose(ausecours.transform.position + Vector3.down * décalage);
             }
         }
-
     }
-
 
     public void Explose(Vector3 centreExplosion)
     {
@@ -36,14 +35,47 @@ public class Unj_Cassable : MonoBehaviour
 
             Vector3 force = direction2D * explosionForce;
             rb.AddForce(force);
-            var tempsDespawnCubes = UnityEngine.Random.Range(2, 4);
-            Destroy(rb.gameObject, tempsDespawnCubes);
 
+            var tempsDespawnCubes = UnityEngine.Random.Range(0.5f, 4f);
+            StartCoroutine(ScaleDownAndDestroy(rb.gameObject, tempsDespawnCubes));
         }
 
-        Destroy(gameObject);
+        // Destroy(gameObject); // Casser tronc 
+
+        var components = GetComponents<Component>();
+        foreach (var component in components)
+        {
+            if (component is Unj_Cassable)
+                continue;
+
+            Destroy(component);
+        }
+        Destroy(gameObject, 5f);
+    }
+
+    private IEnumerator ScaleDownAndDestroy(GameObject fragment, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        float scaleDuration = 1f;
+        float elapsed = 0f;
+
+        Vector3 initialScale = fragment.transform.localScale;
+
+        while (elapsed < scaleDuration)
+        {
+            elapsed += Time.deltaTime;
+
+            float scale = Mathf.Lerp(1f, 0f, elapsed / scaleDuration);
+            fragment.transform.localScale = initialScale * scale;
+
+            yield return null;
+        }
+
+        Destroy(fragment); // Casser fragments
     }
 }
+
 
 
 
